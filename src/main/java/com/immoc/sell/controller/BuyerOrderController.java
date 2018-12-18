9941e1f3c9999f3d739ecae949c5f277d5bf929e
396220003@qq.com <396220003@qq.com>
2018-12-19 00:11:49 +0800

    添加文件

--- src/main/java/com/immoc/sell/controller/BuyerOrderController.java
+++ src/main/java/com/immoc/sell/controller/BuyerOrderController.java
@@ -9,14 +9,18 @@ import com.immoc.sell.form.OrderForm;
 import com.immoc.sell.service.OrderService;
 import com.immoc.sell.utils.ResultVOUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.criterion.Order;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -28,6 +32,7 @@ public class BuyerOrderController {
     private OrderService orderService;
 
     //    创建订单
+    @PostMapping("/create")
     public ResultVO<Map<String, String>> create(@Valid OrderForm orderForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             log.error("【创建订单】 参数不正确， orderForm={}", orderForm);
@@ -48,8 +53,19 @@ public class BuyerOrderController {
         return ResultVOUtil.success(map);
     }
 
-//    订单列表
-
+    //    订单列表
+    @GetMapping("/list")
+    public ResultVO<List<OrderDTO>> list(@RequestParam("openid")String openid,
+                                         @RequestParam(value = "page", defaultValue = "0") Integer page,
+                                         @RequestParam(value = "size", defaultValue = "10") Integer size) {
+        if (StringUtils.isEmpty(openid)) {
+            log.error("【查询订单列表】 openid为空");
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+        PageRequest request = PageRequest.of(page, size);
+        Page<OrderDTO> orderDTOPage = orderService.findList(openid, request);
+        return ResultVOUtil.success(orderDTOPage.getContent());
+    }
 //    订单详情
 
 //    取消订单
--- src/main/java/com/immoc/sell/dto/OrderDTO.java
+++ src/main/java/com/immoc/sell/dto/OrderDTO.java
@@ -1,8 +1,10 @@
 package com.immoc.sell.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.immoc.sell.dataobject.OrderDetail;
 import com.immoc.sell.enums.OrderStatusEnum;
 import com.immoc.sell.enums.PayStatusEnum;
+import com.immoc.sell.utils.serializer.Date2LongSerializer;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -28,8 +30,10 @@ public class OrderDTO {
 
     private Integer payStatus;
 
+    @JsonSerialize(using = Date2LongSerializer.class)
     private Date createTime;
 
+    @JsonSerialize(using = Date2LongSerializer.class)
     private Date updateTime;
 
     List<OrderDetail> orderDetailList;
